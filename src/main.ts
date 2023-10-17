3efe5d03c355da2c44b64d985119aa3d480d5f7f
Neuron <phyowailin2004@proton.me>
2023-10-18 03:00:18 +0630

    Add complete functional scripts.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,162 @@
+import colorConvert from "color-convert";
 import "./style.css";
+
+// # Predefined functions
+// ----------
+
+function updatePanelCanvas(canvas: HTMLCanvasElement, hue: number) {
+  hue = clampNumber(0, hue, 360);
+
+  const context = canvas.getContext("2d");
+
+  if (context == null) {
+    return;
+  }
+
+  const { width, height } = canvas;
+  const imageData = generateCanvasImageData(hue, width, height);
+
+  context.putImageData(imageData, 0, 0);
+}
+
+function generateCanvasImageData(hue: number, width: number, height: number) {
+  const arr = new Uint8ClampedArray(width * height * 4);
+
+  for (let x = 0; x < width; x++) {
+    const saturation = x / width;
+
+    for (let y = 0; y < height; y++) {
+      const value = 1 - y / height;
+
+      const index = (y * width + x) * 4;
+
+      const rgb = colorConvert.hsv.rgb([hue, saturation * 100, value * 100]);
+
+      arr[index] = rgb[0];
+      arr[index + 1] = rgb[1];
+      arr[index + 2] = rgb[2];
+      arr[index + 3] = 255;
+    }
+  }
+
+  return new ImageData(arr, width, height);
+}
+
+function clampNumber(min: number, prefer: number, max: number) {
+  return Math.min(Math.max(min, prefer), max);
+}
+
+function onEveryAnimationFrame(callback: FrameRequestCallback) {
+  let stop = false;
+
+  const innerFn: FrameRequestCallback = (time) => {
+    if (stop) return;
+    callback(time), requestAnimationFrame(innerFn);
+  };
+  requestAnimationFrame(innerFn);
+
+  return () => {
+    stop = true;
+  };
+}
+
+const $ = (selector: string) => document.querySelector(selector);
+
+// ----------
+
+const panelCanvas = $(".panel-canvas") as HTMLCanvasElement;
+const panelHandle = $(".panel-handle") as HTMLElement;
+const panel = $(".panel") as HTMLElement;
+const preview = $(".preview") as HTMLElement;
+const hueInput = $("input.hue-input") as HTMLInputElement;
+
+let previousHue: number = clampNumber(0, +hueInput.value, 360);
+let currentHue: number = previousHue;
+
+let handlePosition = { x: 0, y: 0 };
+
+let isHueFirstInput = true;
+let hueInputTimeoutId: number | undefined;
+
+hueInput.addEventListener("input", () => {
+  currentHue = clampNumber(0, +hueInput.value, 360);
+
+  if (isHueFirstInput) {
+    const stop = onEveryAnimationFrame(() => {
+      if (previousHue !== currentHue) {
+        previousHue = currentHue;
+        updatePanelCanvas(panelCanvas, currentHue);
+        pickColor();
+      }
+    });
+
+    clearTimeout(hueInputTimeoutId);
+    hueInputTimeoutId = setTimeout(() => {
+      isHueFirstInput = true;
+      stop();
+    }, 100);
+  }
+
+  isHueFirstInput = false;
+
+  updateHueInputThumbColor();
+});
+
+panel.addEventListener("pointerdown", () => {
+  const rect = panel.getBoundingClientRect();
+
+  const onMouseMove = (event: MouseEvent | TouchEvent) => {
+    if (event instanceof MouseEvent) {
+      handlePosition.x = clampNumber(0, event.x - rect.x, rect.width);
+      handlePosition.y = clampNumber(0, event.y - rect.y, rect.height);
+    } else {
+      const touch = event.targetTouches[0];
+      handlePosition.x = clampNumber(0, touch.clientX - rect.x, rect.width);
+      handlePosition.y = clampNumber(0, touch.clientY - rect.y, rect.height);
+
+      event.preventDefault();
+    }
+
+    panelHandle.style.setProperty("left", handlePosition.x + "px");
+    panelHandle.style.setProperty("top", handlePosition.y + "px");
+
+    pickColor();
+  };
+
+  const onMouseUp = () => {
+    window.removeEventListener("mousemove", onMouseMove);
+    window.removeEventListener("touchmove", onMouseMove);
+  };
+
+  window.addEventListener("mousemove", onMouseMove, { passive: false });
+  window.addEventListener("touchmove", onMouseMove, { passive: false });
+  window.addEventListener("pointerup", onMouseUp, { once: true });
+});
+
+function updateHueInputThumbColor() {
+  hueInput.style.setProperty(
+    "--thumb-color",
+    `hsl(${currentHue}deg, 100%, 50%)`
+  );
+}
+
+function pickColor() {
+  const panelRect = panel.getBoundingClientRect();
+
+  const x = handlePosition.x / panelRect.width;
+  const y = handlePosition.y / panelRect.height;
+
+  const saturation = x * 100;
+  const value = 100 - y * 100;
+
+  const rgb = colorConvert.hsv.rgb([currentHue, saturation, value]);
+
+  const pickedColor = `rgb(${rgb})`;
+
+  preview.style.setProperty("background-color", pickedColor);
+  // colorValue.textContent = pickedColor;
+}
+
+updatePanelCanvas(panelCanvas, currentHue);
+pickColor();
+updateHueInputThumbColor();
